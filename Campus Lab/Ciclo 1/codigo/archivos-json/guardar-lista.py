eb92f982815af47e5347a273bd2f858b2f7a2127
carloshrueda <8718422+carloshrueda@users.noreply.github.com>
2023-07-12 19:42:34 -0500

    creación del archivo json

--- Campus Lab/Ciclo 1/codigo/archivos-json/guardar-lista.py
+++ Campus Lab/Ciclo 1/codigo/archivos-json/guardar-lista.py
@@ -1,13 +1,15 @@
 # Crear archivo json con una estructura de datos lista
 
-
 import json
 
 lista = [10, 20, 30, 40, 60]
 
 with open("Ciclo 1/codigo/archivos-json/lista.json", "w") as archivo:
     json.dump(lista, archivo)
+    print("Se ha escrito en disco")
     
 if not archivo.closed:
     print("Cerrando archivo")
     archivo.close()
+    
+print("Se ha cerrado el archivo")
