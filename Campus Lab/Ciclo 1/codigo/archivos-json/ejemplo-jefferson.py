b55a61975c0eae54d76d5bb0b6428f9406df8273
carloshrueda <8718422+carloshrueda@users.noreply.github.com>
2023-07-15 09:40:57 -0500

    contenido tema JSON

--- Campus Lab/Ciclo 1/codigo/archivos-json/ejemplo-jefferson.py
+++ Campus Lab/Ciclo 1/codigo/archivos-json/ejemplo-jefferson.py
@@ -1,17 +1,23 @@
-{
+cole = {
     "6-1":{
-        "14": {
+        14: {
             "nombre":"carlos",
             "sexo":"m",
-            "nota":null,
+            "nota":None,
             "prom":0
             
         },
-        "15": {
+        15: {
             "nombre":"diana",
             "sexo":"f",
-            "nota":null,
+            "nota":None,
             "prom":0
         }
     }
 }
+
+listnombres = ["carlos", "diana"]
+listnombres.sort()
+
+for nombre in listnombres:
+    pass
--- Campus Lab/Ciclo 1/codigo/archivos-json/guardar-lista.py
+++ Campus Lab/Ciclo 1/codigo/archivos-json/guardar-lista.py
@@ -2,9 +2,14 @@
 
 import json
 
-lista = [10, 20, 30, 40, 60]
+def myfuncion(dato):
+    return dato * 2
+
+lista = [10, 20, 30, 40, 60, 70]
+
 
 with open("Campus Lab/Ciclo 1/codigo/archivos-json/lista.json", "w") as archivo:
+    lista.append(myfuncion(60))
     json.dump(lista, archivo)
     print("Se ha escrito en disco")
     
